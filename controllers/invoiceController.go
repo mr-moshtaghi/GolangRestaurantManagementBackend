fa2079909005a4e6c5e75082f31643c41f30946b
ahmad_ali_moshtaghi <moshtaghi@gmail.com>
2023-06-29 10:58:45 +0330

    create base InvoiceController

--- controllers/invoiceController.go
+++ controllers/invoiceController.go
@@ -1,1 +1,29 @@
-package controllers
+package controller
+
+import (
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+	"restaurant-management/database"
+)
+
+var invoiceCollection *mongo.Collection = database.OpenCollection(database.Client, "invoice")
+
+func GetInvoices() gin.HandlerFunc {
+	return func(c *gin.Context) {
+	}
+}
+
+func GetInvoice() gin.HandlerFunc {
+	return func(c *gin.Context) {
+	}
+}
+
+func CreateInvoice() gin.HandlerFunc {
+	return func(c *gin.Context) {
+	}
+}
+
+func UpdateInvoice() gin.HandlerFunc {
+	return func(c *gin.Context) {
+	}
+}
