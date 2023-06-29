5f1e1e79c39648278f0f1b455c7600c9a4de7221
ahmad_ali_moshtaghi <moshtaghi@gmail.com>
2023-06-29 11:00:45 +0330

    create base MenuController

--- controllers/menueController.go
+++ controllers/menueController.go
@@ -1,1 +1,35 @@
-package controllers
+package controller
+
+import (
+	"restaurant-management/database"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var menuCollection *mongo.Collection = database.OpenCollection(database.Client, "menu")
+
+func GetMenus() gin.HandlerFunc {
+	return func(c *gin.Context) {
+	}
+}
+
+func GetMenu() gin.HandlerFunc {
+	return func(c *gin.Context) {
+	}
+}
+
+func CreateMenu() gin.HandlerFunc {
+	return func(c *gin.Context) {
+	}
+}
+
+func inTimeSpan(start, end, check time.Time) bool {
+	return start.After(time.Now()) && end.After(start)
+}
+
+func UpdateMenu() gin.HandlerFunc {
+	return func(c *gin.Context) {
+	}
+}
