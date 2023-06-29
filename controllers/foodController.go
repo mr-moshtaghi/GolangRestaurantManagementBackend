92d48a02bbfe58bc4c51e86d5853a592d0293387
ahmad_ali_moshtaghi <moshtaghi@gmail.com>
2023-06-29 10:53:44 +0330

    create base FoodController

--- controllers/foodController.go
+++ controllers/foodController.go
@@ -1,1 +1,43 @@
-package controllers
+package controller
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"go.mongodb.org/mongo-driver/mongo"
+	"math"
+	"restaurant-management/database"
+)
+
+var foodCollection *mongo.Collection = database.OpenCollection(database.Client, "food")
+var validate = validator.New()
+
+func GetFoods() gin.HandlerFunc {
+	return func(c *gin.Context) {
+	}
+}
+
+func GetFood() gin.HandlerFunc {
+	return func(c *gin.Context) {
+	}
+}
+
+func CreateFood() gin.HandlerFunc {
+	return func(c *gin.Context) {
+	}
+}
+
+func UpdateFood() gin.HandlerFunc {
+	return func(c *gin.Context) {
+	}
+}
+
+func round(num float64) int {
+	return int(num + math.Copysign(0.5, num))
+}
+
+func toFixed(num float64, precision int) float64 {
+	output := math.Pow(10, float64(precision))
+	return float64(round(num*output)) / output
+}
+
+
