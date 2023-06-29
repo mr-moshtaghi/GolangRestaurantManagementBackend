7623d50b99795d481d33e1c01d60c012398c5083
ahmad_ali_moshtaghi <moshtaghi@gmail.com>
2023-06-29 11:03:30 +0330

    create base OrderController

--- controllers/orderController.go
+++ controllers/orderController.go
@@ -1,1 +1,40 @@
-package controllers
+package controller
+
+import (
+	"context"
+	"restaurant-management/database"
+	"time"
+
+	"github.com/gin-gonic/gin"
+
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+
+var orderCollection *mongo.Collection = database.OpenCollection(database.Client, "order")
+
+func GetOrders() gin.HandlerFunc {
+	return func(c *gin.Context) {
+	}
+}
+
+func GetOrder() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func CreateOrder() gin.HandlerFunc {
+	return func(c *gin.Context) {
+	}
+}
+
+func UpdateOrder() gin.HandlerFunc {
+	return func(c *gin.Context) {
+	}
+}
+
+//func OrderItemOrderCreator(order models.Order) string {
+//
+//}
