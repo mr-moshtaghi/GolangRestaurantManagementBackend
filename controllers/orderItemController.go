fd81594a5b8d37ccceb7af0e24a770fdd76c38bc
ahmad_ali_moshtaghi <moshtaghi@gmail.com>
2023-06-29 11:05:19 +0330

    create base OrderItemController

--- controllers/orderItemController.go
+++ controllers/orderItemController.go
@@ -1,1 +1,39 @@
-package controllers
+package controller
+
+import (
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"restaurant-management/database"
+)
+
+var orderItemCollection *mongo.Collection = database.OpenCollection(database.Client, "orderItem")
+
+func GetOrderItems() gin.HandlerFunc {
+	return func(c *gin.Context) {
+	}
+}
+
+func GetOrderItemsByOrder() gin.HandlerFunc {
+	return func(c *gin.Context) {
+	}
+}
+
+func ItemsByOrder(id string) (OrderItems []primitive.M, err error) {
+
+}
+
+func GetOrderItem() gin.HandlerFunc {
+	return func(c *gin.Context) {
+	}
+}
+
+func UpdateOrderItem() gin.HandlerFunc {
+	return func(c *gin.Context) {
+	}
+}
+
+func CreateOrderItem() gin.HandlerFunc {
+	return func(c *gin.Context) {
+	}
+}
