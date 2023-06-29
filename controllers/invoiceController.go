1d9d376510ccc27e19974692c7811332180b6b9f
ahmad_ali_moshtaghi <moshtaghi@gmail.com>
2023-06-29 21:08:37 +0330

    update GetInvoices

--- controllers/invoiceController.go
+++ controllers/invoiceController.go
@@ -1,8 +1,12 @@
 package controller
 
 import (
+	"context"
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
+	"log"
+	"net/http"
 	"restaurant-management/database"
 	"time"
 )
@@ -22,6 +26,19 @@ type InvoiceViewFormat struct {
 
 func GetInvoices() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+
+		result, err := invoiceCollection.Find(context.TODO(), bson.M{})
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while listing invoice items"})
+		}
+
+		var allInvoices []bson.M
+		if err = result.All(ctx, &allInvoices); err != nil {
+			log.Fatal(err)
+		}
+		c.JSON(http.StatusOK, allInvoices)
 	}
 }
 
