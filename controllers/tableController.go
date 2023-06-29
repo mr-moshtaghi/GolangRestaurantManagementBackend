b7f6eb35522ca9cd262b2cd31ba7c20f76d0c265
ahmad_ali_moshtaghi <moshtaghi@gmail.com>
2023-06-29 11:06:27 +0330

    create base TableController

--- controllers/tableController.go
+++ controllers/tableController.go
@@ -1,1 +1,31 @@
-package controllers
+package controller
+
+import (
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+	"restaurant-management/database"
+)
+
+var tableCollection *mongo.Collection = database.OpenCollection(database.Client, "table")
+
+func GetTables() gin.HandlerFunc {
+	return func(c *gin.Context) {
+	}
+}
+
+func GetTable() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func CreateTable() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func UpdateTable() gin.HandlerFunc {
+	return func(c *gin.Context) {
+	}
+}
