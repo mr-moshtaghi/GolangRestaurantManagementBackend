7ea74114c426f98dd9d8d08ea26358bdfae32c1d
ahmad_ali_moshtaghi <moshtaghi@gmail.com>
2023-06-29 10:22:38 +0330

    update

--- main.go
+++ main.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+	"os"
+
+	"restaurant-management/database"
+	middleware "restaurant-management/middleware"
+	routes "restaurant-management/routes"
+)
+
+var foodCollection *mongo.Collection = database.OpenCollection(database.Client, "food")
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+	routes.UserRouter(router)
+	router.Use(middleware.Auth)
+}
