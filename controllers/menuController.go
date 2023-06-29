bb8c5ab51081e171538725c32679d9fce1530d55
ahmad_ali_moshtaghi <moshtaghi@gmail.com>
2023-06-29 16:04:11 +0330

    update UpdateMenu

--- controllers/menuController.go
+++ controllers/menuController.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"net/http"
 	"restaurant-management/database"
 	"restaurant-management/models"
@@ -75,5 +76,62 @@ func inTimeSpan(start, end, check time.Time) bool {
 
 func UpdateMenu() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		var menu models.Menu
+
+		if err := c.BindJSON(&menu); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		menuId := c.Param("menu_id")
+		filter := bson.M{"manuu_id": menuId}
+
+		var updateObj primitive.D
+
+		if menu.StartDate != nil && menu.EndDate != nil {
+			if !inTimeSpan(*menu.StartDate, *menu.EndDate, time.Now()) {
+				msg := "kindly retype the time"
+				c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+				defer cancel()
+				return
+			}
+
+			updateObj = append(updateObj, bson.E{"start_date", menu.StartDate})
+			updateObj = append(updateObj, bson.E{"end_date", menu.EndDate})
+
+			if menu.Name != "" {
+				updateObj = append(updateObj, bson.E{"name", menu.Name})
+			}
+			if menu.Category != "" {
+				updateObj = append(updateObj, bson.E{"name", menu.Category})
+			}
+
+			menu.UpdatedAt, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+			updateObj = append(updateObj, bson.E{"updated_at", menu.UpdatedAt})
+
+			upsert := true
+
+			opt := options.UpdateOptions{
+				Upsert: &upsert,
+			}
+
+			result, err := menuCollection.UpdateOne(
+				ctx,
+				filter,
+				bson.D{
+					{"$set", updateObj},
+				},
+				&opt,
+			)
+
+			if err != nil {
+				msg := "Menu update failed"
+				c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+			}
+
+			defer cancel()
+			c.JSON(http.StatusOK, result)
+		}
 	}
 }
