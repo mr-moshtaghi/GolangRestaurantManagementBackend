c1dac0d46910ef2f990135e49ea0c297ff01f565
ahmad_ali_moshtaghi <moshtaghi@gmail.com>
2023-06-29 10:50:59 +0330

    create base UserController

--- controllers/userController.go
+++ controllers/userController.go
@@ -1,1 +1,36 @@
-package controllers
+package controller
+
+import (
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+	"restaurant-management/database"
+)
+
+var userCollection *mongo.Collection = database.OpenCollection(database.Client, "user")
+
+func GetUsers() gin.HandlerFunc {
+	return func(c *gin.Context) {
+	}
+}
+
+func GetUser() gin.HandlerFunc {
+	return func(c *gin.Context) {
+	}
+}
+
+func SignUp() gin.HandlerFunc {
+	return func(c *gin.Context) {
+	}
+}
+
+func Login() gin.HandlerFunc {
+	return func(c *gin.Context) {
+	}
+}
+
+func HashPassword(password string) string {
+
+}
+
+func VerifyPassword(userPassword string, providedPassword string) (bool, string) {
+}
