2ae167be1f31a80e62e3e93ed55f60422c6c4376
ahmad_ali_moshtaghi <moshtaghi@gmail.com>
2023-06-30 16:51:32 +0330

    update GetOrderItems

--- controllers/orderItemController.go
+++ controllers/orderItemController.go
@@ -1,16 +1,42 @@
 package controller
 
 import (
+	"context"
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"log"
+	"net/http"
 	"restaurant-management/database"
+	"restaurant-management/models"
+	"time"
 )
+
+type OrderItemPack struct {
+	TableId    *string
+	OrderItems []models.OrderItem
+}
 
 var orderItemCollection *mongo.Collection = database.OpenCollection(database.Client, "orderItem")
 
 func GetOrderItems() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+
+		result, err := orderItemCollection.Find(context.TODO(), bson.M{})
+
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while listing ordered items"})
+			return
+		}
+		var allOrderItems []bson.M
+		if err = result.All(ctx, &allOrderItems); err != nil {
+			log.Fatal(err)
+			return
+		}
+		c.JSON(http.StatusOK, allOrderItems)
 	}
 }
 
