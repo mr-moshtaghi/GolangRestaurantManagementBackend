e7225d3eb453d3437ec160315ee540dae98dfbb2
ahmad_ali_moshtaghi <moshtaghi@gmail.com>
2023-06-30 17:01:10 +0330

    update CreateOrderItem

--- controllers/orderItemController.go
+++ controllers/orderItemController.go
@@ -83,5 +83,47 @@ func UpdateOrderItem() gin.HandlerFunc {
 
 func CreateOrderItem() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+
+		var orderItemPack OrderItemPack
+		var order models.Order
+
+		if err := c.BindJSON(&orderItemPack); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		order.OrderDate, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+
+		orderItemsToBeInserted := []interface{}{}
+		order.TableId = orderItemPack.TableId
+		order_id := OrderItemOrderCreator(order)
+
+		for _, orderItem := range orderItemPack.OrderItems {
+			orderItem.OrderId = order_id
+
+			validationErr := validate.Struct(orderItem)
+
+			if validationErr != nil {
+				c.JSON(http.StatusBadRequest, gin.H{"error": validationErr.Error()})
+				return
+			}
+			orderItem.ID = primitive.NewObjectID()
+			orderItem.CreatedAt, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+			orderItem.UpdatedAt, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+			orderItem.OrderItemId = orderItem.ID.Hex()
+			var num = toFixed(*orderItem.UnitPrice, 2)
+			orderItem.UnitPrice = &num
+			orderItemsToBeInserted = append(orderItemsToBeInserted, orderItem)
+		}
+
+		insertedOrderItems, err := orderItemCollection.InsertMany(ctx, orderItemsToBeInserted)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer cancel()
+
+		c.JSON(http.StatusOK, insertedOrderItems)
 	}
 }
